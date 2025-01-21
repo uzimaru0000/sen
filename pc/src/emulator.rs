bd317106ff47bf51789ad5436d636efe8842adbe
uzimaru0000 <shuji365630@gmail.com>
2025-01-21 22:59:32 +0900

    fix sdl2 size

--- pc/src/emulator.rs
+++ pc/src/emulator.rs
@@ -11,13 +11,13 @@ impl Sdl2Emulator {
         let sdl_context = sdl2::init().unwrap();
         let video_subsystem = sdl_context.video().unwrap();
         let window = video_subsystem
-            .window("SEN", (256.0 * 5.0) as u32, (240.0 * 5.0) as u32)
+            .window("SEN", (256.0 * 2.0) as u32, (240.0 * 2.0) as u32)
             .position_centered()
             .build()
             .unwrap();
         let mut canvas = window.into_canvas().present_vsync().build().unwrap();
         let event_pump = sdl_context.event_pump().unwrap();
-        canvas.set_scale(5.0, 5.0).unwrap();
+        canvas.set_scale(2.0, 2.0).unwrap();
         let creator = canvas.texture_creator();
 
         let speaker = SdlSpeaker::new(&sdl_context);
